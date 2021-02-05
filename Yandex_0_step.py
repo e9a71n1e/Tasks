ebf47a61723599b4e46fe52eb5f7e38b754cbff5
e9a71n1e <35028644+e9a71n1e@users.noreply.github.com>
2021-02-05 21:36:14 +0300

    Update Yandex_0_step.py

--- Yandex_0_step.py
+++ Yandex_0_step.py
@@ -5,7 +5,7 @@ sweets = [candy for candy in input("Enter space-separated candy names (without q
 from functools import reduce
 from math import gcd  # greatest common divisor
 
-# we need group by candy names and get greatest common divisor to get a maximum number of friends
+# we need grouping by candy names and get greatest common divisor to get a maximum number of friends
 # reduce here is just for pairwise computation of gcd
 
 print(reduce(gcd, [sweets.count(x) for x in set(sweets)]))
